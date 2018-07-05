72206b5701bc61f64c378d0044680bda8cc61e18
lxl <luo1762186@163.com>
2018-07-05 12:38:17 +0800

    添加 从文章列表页面提取每篇文章url

--- bole/spiders/jobbole.py
+++ bole/spiders/jobbole.py
@@ -10,8 +10,34 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/114185//']
 
     def parse(self, response):
-        #获取
+        """
+        1、获取文章列表url，交给scrapy解析
+        2、获取下一页url交给scrapy下载，下载完parse
+        3、使用xpath解析
+        4、当获取的url不完整的时候，一般response.url+post_url拼接完整url地址
 
+        :param response:
+        :return:
+        """
+        #获取文章列表url
+        article_notes=response.xpath("//div[@class='post floated-thumb']/div[@class='post-thumb']/a")
+        for article_note in article_notes:
+            #每篇文章url
+            article_url=article_note.xpath("./@href").extract_first()
+            #每篇文章封面图url
+            article_img_url=article_note.xpath("./img/@src").extract_first()
+
+            #callback回调函数，回调到parse_detail函数，对每个网页进行解析
+            yield Request(url=parse.urljoin(response.url,article_url),meta={"front_image_url":article_img_url},callback=self.parse_detail)
+
+        #下一页
+        next_url=response.xpath("//a[@class='next page-numbers']/@href").extract_first()
+        if next_url:
+            yield Request(url=parse.urljoin(response.url,next_url),callback=self.parse)
+
+    def parse_detail(self, response):
+        #文章封面图,通过回调函数callback，从parse函数中拿到图片url
+        front_image=response.meta.get("front_image_url","")
 
 
 
