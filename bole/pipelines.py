cb8bd2da1810f57ed7c652b75d33c65a010f552c
lxl <luo1762186@163.com>
2018-07-07 17:58:03 +0800

    添加获取存入图片路径class 类 添加同步插入数据库

--- bole/pipelines.py
+++ bole/pipelines.py
@@ -7,6 +7,11 @@
 import codecs
 import json
 from scrapy.exporters import JsonItemExporter
+import pymysql
+from scrapy.pipelines.images import ImagesPipeline
+
+
+
 
 class BolePipeline(object):
     def process_item(self, item, spider):
@@ -28,7 +33,7 @@ class JsonWithEncodingPipeline(object):
     #重写item处理
     def process_item(self,item,spider):
         # 处理字典，输出中文
-        lines=json.dumps(dict(item),ensure_ascii=False)+"\n"
+        lines=json.dumps(dict(item),ensure_ascii=False)+'\n'
         #一行数据写入
         self.file.write(lines)
         return item
@@ -51,3 +56,42 @@ class JsonExportPipline(object):
     def process_item(self,item,spider):
         self.exporter.export_item(item)
         return item
+
+
+#获取图片文件的路径
+class ArticleImagePipeline(ImagesPipeline):
+    #继承ImagesPipeline 获取保存路径必须重置item_completed函数
+    def item_completed(self,results,item,info):
+        if 'front_image_url' in item:       #判断路径是否存在
+            for ok,value in results:
+                image_file_path=value["path"]
+                item["front_image_path"]=image_file_path
+                return item
+
+
+
+
+"""
+存入数据库,一种是同步，一种是异步
+1、同步操作一条语句不执行完不会执行下一条语句。但是我们scrapy的解析速度很快如果入库的速度很慢的话就会造成阻塞
+"""
+#同步写入mysql数据库
+class mysqlExportPipline(object):
+    def __init__(self):
+        self.conn=pymysql.connect('localhost','root','123456','Crawler',charset='utf8',use_unicode=True)
+        self.cursor=self.conn.cursor()  #获取连接对象
+
+    def process_item(self,item,spider):
+        insert_sql="""insert into Bole(url_object_id,title,url,front_image_url,front_image_path,
+create_date,pare_num,collect_num,comment_num,tags)VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                """
+        self.cursor.execute(insert_sql, (item["url_object_id"], item["title"], item["url"], item["front_image_url"], item["front_image_path"],
+        item["create_date"], item["pare_num"], item["collect_num"], item["comment_num"],item["tags"]))
+
+        self.conn.commit()
+
+        #可以添加判断sql执行情况，添加回滚机制rollback()
+
+    def spider_closed(self,spider):
+        self.conn.close()
+
